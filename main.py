1c9c27c8c6953cb5d08545093824216868b672c5
SuekieZA <53234220+suekieza@users.noreply.github.com>
2021-10-02 10:28:20 +0000

    started bot

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Hello World')
+import discord
+import os
+bot_secret = os.environ['BOTTOKEN']
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print(f'We have logged in as {client}.user')
+
+@client.event
+async def on_massage(massage):
+  if massage.author == client.user:
+    return
+
+  if massage.content.startswith('$hello'):
+    await massage.channel.send('Hello')
+
+client.run(bot_secret)
